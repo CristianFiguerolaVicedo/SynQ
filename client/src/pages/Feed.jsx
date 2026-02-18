f38c55457db0c3f6b3434ea0d741941156320ba7
CriFigShe <crifigshe@gmail.com>
2026-02-18 12:45:54 +0100

    Recent messaged. Feed finished

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
-import { dummyPostsData } from '../../public/assets/assets';
+import { assets, dummyPostsData } from '../../public/assets/assets';
 import Loading from '../components/Loading';
 import StoriesBar from '../components/StoriesBar';
 import PostCard from '../components/PostCard';
+import RecentMessages from '../components/RecentMessages';
 
 const Feed = () => {
     const [feeds, setFeeds] = useState([]);
@@ -28,11 +29,18 @@ const Feed = () => {
                 </div>
             </div>
 
-            <div>
-                <div>
-                    <h1>Sponsored</h1>
+            <div className='max-xl:hidden sticky top-0'>
+                <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+                    <h3 className='text-slate-800 fonts-semibold'>Sponsored</h3>
+                    <img 
+                        src={assets.sponsored_img} 
+                        alt="" 
+                        className='w-75 h-50 rounded-md'
+                    />
+                    <p className='text-slate-600'>Email Marketing</p>
+                    <p className="text-slate-400">Empower the flow of people to your website and make yourself the leader of the sector.</p>
                 </div>
-                <h1>Recent messages</h1>
+                <RecentMessages />
             </div>
         </div>
     ) : (
