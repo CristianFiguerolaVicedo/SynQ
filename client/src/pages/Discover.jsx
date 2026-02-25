4112e2627602e02a4f7cd6d5616b698b00168693
CriFigShe <crifigshe@gmail.com>
2026-02-25 12:29:24 +0100

    Discover styles updated

--- client/src/pages/Discover.jsx
+++ client/src/pages/Discover.jsx
@@ -21,21 +21,21 @@ const Discover = () => {
     }
 
     return (
-        <div className='min-h-screen bg-gradient-to-b from-slate-50 to-white'>
+        <div className='min-h-screen bg-[#0F172A]'>
             <div className='max-w-6xl mx-auto p-6'>
                 <div className='mb-8'>
-                    <h1 className='text-3xl font-bold text-slate-900 mb-2'>Discover new people.</h1>
-                    <p className='text-slate-600'>Discover and talk to new people.</p>
+                    <h1 className='text-3xl font-bold text-[#F1F5F9] mb-2'>Discover new people.</h1>
+                    <p className='text-[#94A3B8]'>Discover and talk to new people.</p>
                 </div>
 
-                <div className='mb-8 shadow-md rounded-md border border-slate-200/60 bg-white/80'>
+                <div className='mb-8 shadow-md rounded-md  bg-[#3B82F6]/80'>
                     <div className='p-6'>
                         <div className='relative'>
-                            <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 text-slate-400 w-5 h-5'/>
+                            <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 text-[#F1F5F9] w-5 h-5'/>
                             <input 
                                 type="text" 
                                 placeholder='Search people by name, username, bio, or location...' 
-                                className='pl-10 sm:pl-12 py-2 w-full border border-gray-300 rounded-md max-sm:text-sm' 
+                                className='pl-10 sm:pl-12 py-2 w-full rounded-md max-sm:text-sm border border-[#1F2937] text-[#F1F5F9]' 
                                 onChange={(e) => setInput(e.target.value)} 
                                 value={input} 
                                 onKeyUp={handleSearch}
