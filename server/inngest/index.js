a4e92d581098f16ed6b3c4fa8fde11e92a470c43
CriFigShe <crifigshe@gmail.com>
2026-02-26 13:42:51 +0100

    Name hot fix

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -3,46 +3,47 @@ import User from "../models/User.js";
 
 export const inngest = new Inngest({ id: "synq-app" });
 
-const syncUserCreation = inngest.createFunction(
-    {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created'},
-    async ({event}) => {
-        const {id, first_name, last_name, email_addresses, image_url} = event.data
-        let username = email_addresses[0].email_addresses.split('@')[0]
+async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
 
-        const user = await User.findOne({username})
+    const email = email_addresses?.[0]?.email_address;
 
-        if (user) {
-            username = username + Math.floor(Math.random() * 10000)
-        }
-
-        const userData = {
-            _id: id,
-            email: email_addresses[0].email_addresses,
-            full_name: first_name + " " + last_name,
-            profile_picture: image_url,
-            username
-        }
-
-        await User.create(userData)
+    if (!email) {
+        throw new Error("No email found for Clerk user");
     }
-)
 
-const syncUserUpdate = inngest.createFunction(
-    {id: 'update-user-from-clerk'},
-    {event: 'clerk/user.updated'},
-    async ({event}) => {
-        const {id, first_name, last_name, email_addresses, image_url} = event.data
+    let username = email.split("@")[0];
 
-        const updatedUserData = {
-            email: email_addresses[0].email_addresses,
-            full_name: first_name + ' ' + last_name,
-            profile_picture: image_url
-        }
+    const user = await User.findOne({ username });
 
-        await User.findByIdAndUpdate(id, updatedUserData)
+    if (user) {
+        username = username + Math.floor(Math.random() * 10000);
     }
-)
+
+    const userData = {
+        _id: id,
+        email,
+        full_name: `${first_name} ${last_name}`,
+        profile_picture: image_url,
+        username,
+    };
+
+    await User.create(userData);
+}
+
+async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+    const email = email_addresses?.[0]?.email_address;
+
+    const updatedUserData = {
+        email,
+        full_name: `${first_name} ${last_name}`,
+        profile_picture: image_url,
+    };
+
+    await User.findByIdAndUpdate(id, updatedUserData);
+}
 
 const syncUserDelete = inngest.createFunction(
     {id: 'delete-user-with-clerk'},
